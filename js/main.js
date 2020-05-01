d35ceb63b9b2ca4de3b6f786feb7e4c99d62dd82
broezer <bruce@moerdjiman.com>
2020-05-01 15:10:42 +0200

    update basic fadein animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
+/*
 var waypoint = new Waypoint({
   element: document.getElementById('natuurpodium'),
   handler: function() {
-    console.log('Natuurpodium waypoint triggered')
+    //console.log('Natuurpodium waypoint triggered')
     this.element.classList.add('active');
   },
   offset: '50%'
 })
+*/
+
+var continuousElements = document.getElementsByClassName('continuous-true')
+for (var i = 0; i < continuousElements.length; i++) {
+  new Waypoint({
+    element: continuousElements[i],
+    handler: function() {
+      this.element.classList.add('active');
+    },
+    offset: '50%'
+  })
+}
